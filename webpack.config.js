80f1665c6b1148e744bcecbd2893cff00e16c086
devmathsena <dev.matheussena@gmail.com>
2025-05-02 08:52:13 -0300

    Create webpack.config.js
    Created webpack.config.js and setted babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
