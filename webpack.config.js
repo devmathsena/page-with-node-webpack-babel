3e516cb3d5dc20eb394c6f3039112de53c807673
devmathsena <dev.matheussena@gmail.com>
2025-05-05 23:16:57 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,19 @@ module.exports = {
     entry: {
         index: './src/index.js'
     },
-    mode: 'production',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.min.js'
+        filename: '[name].min.js'
     },
     module: {
         rules: [
             {
-                test: /\.(?:js|mjs|cjs)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        targets: "defaults",
-                        presets: [
-                            ['@babel/preset-env']
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.css$/i,
+                test: /\.css$/,
                 use: ["style-loader", "css-loader"],
+            }, {
+                test: /\.js$/,
+                use: ['babel-loader']
             }
         ]
     }
