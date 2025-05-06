3b8700cea69a3d87d3d1a7509d2b363f36f84e91
devmathsena <dev.matheussena@gmail.com>
2025-05-05 22:49:56 -0300

    Update webpack.config.js
    Setted output in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+const path = require('path')
+
 module.exports = {
     entry: {
         index: './src/index.js'
     },
     mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.min.js'
+    },
     module: {
         rules: [
             {
